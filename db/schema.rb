43210d37ea36ff47165923e326c9e23cf94d4ea9
dunow <dunow33@hotmail.com>
2020-11-06 13:03:38 -0500

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -12,4 +12,14 @@
 
 ActiveRecord::Schema.define(version: 2020_11_02_185047) do
 
+  create_table "tweets", force: :cascade do |t|
+    t.string "content"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+  end
+
 end
